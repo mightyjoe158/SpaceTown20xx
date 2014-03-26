b7bef50f807da9eb2bd73cc6b60b19f4c7d6f743
Samuel DiBella <samuel.dibella@gmail.com>
2014-03-26 04:14:42 -0400

    added builds, including basic title and instruction screen. imported sounds

---
+++ Assets/Scripts/InstructionScreen.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstructionScreen : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return)) {
+			Application.LoadLevel("TestLevel");
+		}
+	}
+}
---
+++ Assets/Scripts/healthDisplayScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class healthDisplayScript : MonoBehaviour {
+	
+	public GameObject avatar;
+	TextMesh mesh;
+	
+	// Use this for initialization
+	void Start () {
+		mesh = GetComponent<TextMesh>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		mesh.text = "HP: " + avatar.GetComponent<weaponScript>().hp;
+	}
+}
